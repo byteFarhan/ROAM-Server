f0e770ebc2c29bb7d090bcae454a1a0a75b0b4f5
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-04 18:45:44 +0600

    Implement functionality to update tourist spot collection data in the database.

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 // const uri = `mongodb://mongo:pvOVforYAvDvFHYRgiMNluhWgagEwMli@roundhouse.proxy.rlwy.net:39431`;
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-b9a10.dliqx6o.mongodb.net/?retryWrites=true&w=majority&appName=cluster-B9A10`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-b9a10.dliqx6o.mongodb.net/?retryWrites=true&w=majority&appName=cluster-B9A10`;
 // console.log(process.env.DB_USER);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const uri =
-  "mongodb+srv://farhanmazumder75:RrIlBw1KEt2dLs4B@cluster0.l6om5es.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const uri =
+//   "mongodb+srv://farhanmazumder75:RrIlBw1KEt2dLs4B@cluster0.l6om5es.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,10 +26,8 @@ const client = new MongoClient(uri, {
 //All Database collections
 // const database = client.db("B9A10-ROAM");
 // const tourist_spotsCollection = database.collection("tourist_spot");
-const tourist_spotsCollection = client.db("ROAM").collection("tourist_spots");
-const countries_tourist_spots = client
-  .db("ROAM")
-  .collection("countries_tourist_spots");
+const tourist_spots = client.db("B9A10-ROAM").collection("tourist_spots");
+// const all_spots = client.db("B9A10-ROAM").collection("all_spots");
 
 async function run() {
   try {
@@ -39,7 +37,7 @@ async function run() {
     // get :: all tourist_spot
     app.get("/tourist_spots", async (req, res) => {
       try {
-        const coursor = tourist_spotsCollection.find();
+        const coursor = tourist_spots.find();
         const result = await coursor.toArray();
         res.send(result);
       } catch (err) {
@@ -55,47 +53,75 @@ async function run() {
         //   console.log(idInt);
         //   console.log(id);
         const query = { _id: new ObjectId(id) };
-        const result = await tourist_spotsCollection.findOne(query);
+        const result = await tourist_spots.findOne(query);
         res.send(result);
       } catch (err) {
         return res.send({ error: true, massage: err.massage });
       }
     });
-    // Post :: insert single tourist spot in countries_tourist_spots collection in DB
-    app.post("/countries_tourist_spots", async (req, res) => {
+    // Post :: insert single tourist spot in tourist_spots collection in DB
+    app.post("/tourist_spots", async (req, res) => {
       try {
         const newTouristSpot = req.body;
         // console.log(newTouristSpot);
-        const result = await countries_tourist_spots.insertOne(newTouristSpot);
+        const result = await tourist_spots.insertOne(newTouristSpot);
         res.send(result);
       } catch (err) {
         return res.send({ error: true, massage: err.massage });
       }
     });
 
-    // Get :: find multiple data from countries_tourist_spots collection
-    app.get("/countries_tourist_spots", async (req, res) => {
-      try {
-        const result = await countries_tourist_spots.find().toArray();
-        res.send(result);
-      } catch (err) {
-        return res.send({ error: true, massage: err.massage });
-      }
+    // PUT :: update single tourist spot in tourist_spots collection in DB
+    app.put("/tourist_spots/:id", async (req, res) => {
+      const id = req.params.id;
+      const getSpot = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedSpot = {
+        $set: {
+          touristsSpotName: getSpot.touristsSpotName,
+          countryName: getSpot.countryName,
+          travelTime: getSpot.travelTime,
+          seasonality: getSpot.seasonality,
+          totalVisitorsPerYear: getSpot.totalVisitorsPerYear,
+          departure: getSpot.departure,
+          location: getSpot.location,
+          age: getSpot.age,
+          description: getSpot.description,
+          image: getSpot.image,
+          cost: getSpot.cost,
+        },
+      };
+      const result = await tourist_spots.updateOne(
+        filter,
+        updatedSpot,
+        options
+      );
+      res.send(result);
     });
+    // Get :: find multiple data from countries_tourist_spots collection
+    // app.get("/all_spots", async (req, res) => {
+    //   try {
+    //     const result = await all_spots.find().toArray();
+    //     res.send(result);
+    //   } catch (err) {
+    //     return res.send({ error: true, massage: err.massage });
+    //   }
+    // });
 
     // Get :: find single data from countries_tourist_spots collection
-    app.get("/countries_tourist_spots/:id", async (req, res) => {
-      try {
-        const id = req.params.id;
-        //   console.log(id);
-        const query = { _id: new ObjectId(id) };
-        const result = await countries_tourist_spots.findOne(query);
-        res.send(result);
-      } catch (err) {
-        console.log(err);
-        return res.send({ error: true, massage: err.massage });
-      }
-    });
+    // app.get("/all_spots/:id", async (req, res) => {
+    //   try {
+    //     const id = req.params.id;
+    //     //   console.log(id);
+    //     const query = { _id: new ObjectId(id) };
+    //     const result = await all_spots.findOne(query);
+    //     res.send(result);
+    //   } catch (err) {
+    //     console.log(err);
+    //     return res.send({ error: true, massage: err.massage });
+    //   }
+    // });
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
