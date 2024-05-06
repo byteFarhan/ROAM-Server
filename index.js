003eaac7960c42f2052aa02c031c672a017c4248
farhan-mazumder <farhanmazumder75@gmail.com>
2024-05-06 14:11:16 +0600

    Implement an api to get countries data from countriesCollection in database.

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const prot = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//functions
+function capitalizeFirstLetter(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 // const uri = `mongodb://mongo:pvOVforYAvDvFHYRgiMNluhWgagEwMli@roundhouse.proxy.rlwy.net:39431`;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-b9a10.dliqx6o.mongodb.net/?retryWrites=true&w=majority&appName=cluster-B9A10`;
 // console.log(process.env.DB_USER);
@@ -27,6 +32,8 @@ const client = new MongoClient(uri, {
 // const database = client.db("B9A10-ROAM");
 // const tourist_spotsCollection = database.collection("tourist_spot");
 const tourist_spots = client.db("B9A10-ROAM").collection("tourist_spots");
+const countriesCollection = client.db("B9A10-ROAM").collection("countries");
+const testimonialCollection = client.db("B9A10-ROAM").collection("testimonial");
 // const all_spots = client.db("B9A10-ROAM").collection("all_spots");
 
 async function run() {
@@ -39,8 +46,11 @@ async function run() {
       try {
         let query = {};
         if (req.query.email) {
-          console.log(req.query.email);
+          //   console.log(req.query.email);
           query = { userEmail: req.query.email };
+        } else if (req.query.country) {
+          // console.log(capitalizeFirstLetter(req.query.country));
+          query = { countryName: capitalizeFirstLetter(req.query.country) };
         }
         // const options = {
         //   projection: {},
@@ -59,7 +69,7 @@ async function run() {
         const id = req.params.id;
         //   const idInt = parseInt(id);
         //   console.log(idInt);
-        console.log(id);
+        // console.log(id);
         const query = { _id: new ObjectId(id) };
         const result = await tourist_spots.findOne(query);
         res.send(result);
@@ -110,11 +120,30 @@ async function run() {
 
     // DELETE :: delete single data from tourist_spots collection
     app.delete("/tourist_spots/:id", async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const query = { _id: new ObjectId(id) };
-      const result = await tourist_spots.deleteOne(query);
-      res.send(result);
+      try {
+        const id = req.params.id;
+        //   console.log(id);
+        const query = { _id: new ObjectId(id) };
+        const result = await tourist_spots.deleteOne(query);
+        res.send(result);
+      } catch (err) {
+        return res.send({ error: true, massage: err.massage });
+      }
+    });
+
+    // Get :: find multiple && all datas from countries collection in DB
+    app.get("/countries", async (req, res) => {
+      try {
+        let query = {};
+        if (req.query.country) {
+          // console.log(capitalizeFirstLetter(req.query.country));
+          query = { countryName: capitalizeFirstLetter(req.query.country) };
+        }
+        const result = await countriesCollection.find(query).toArray();
+        res.send(result);
+      } catch (err) {
+        return res.send({ error: true, massage: err.massage });
+      }
     });
 
     // Get :: find multiple data from tourist_spots collection with user email
